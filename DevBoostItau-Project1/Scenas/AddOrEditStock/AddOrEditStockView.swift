4b659ebc241e6f409b525f08087ce49d420c2107
Cardozo <caio@hanzo.com.br>
2020-09-12 14:49:42 -0300

    conflict

---
+++ DevBoostItau-Project1/Scenas/AddOrEditStock/AddOrEditStockView.swift
@@ -0,0 +1,266 @@
+//
+//  AddOrEditStockView.swift
+//  DevBoostItau-Project1
+//
+//  Created by Caio Cardozo on 12/09/20.
+//  Copyright © 2020 DevBoost-Itau. All rights reserved.
+//
+
+import UIKit
+
+protocol AddOrEditStockViewDelegate: AnyObject {
+    func didTapCloseButton()
+    func didTapInvestOrSaveButton()
+}
+
+final class AddOrEditStockView: BaseView {
+    weak var delegate: AddOrEditStockViewDelegate?
+    
+    let contentView: UIView = {
+        let contentView = UIView(frame: .zero)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        return contentView
+    }()
+    
+    let scrollView: UIScrollView = {
+        let scrollView = UIScrollView(frame: .zero)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.keyboardDismissMode = .interactive
+        return scrollView
+    }()
+    let closeButton: UIButton = {
+        let button = UIButton()
+        button.isUserInteractionEnabled = true
+        button.setImage(UIImage(named: "close"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    let titleLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.text = "Preecha as informações sobre a nova compra de ações"
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        label.textColor = UIColor.black
+        label.font = UIFont.systemFont(ofSize: 24.0)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    let stockStackView: UIStackView = {
+        let stackView = UIStackView(frame: .zero)
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.spacing = 5
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    let stockLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.text = "Ativo"
+        label.numberOfLines = 1
+        label.textAlignment = .left
+        label.textColor = UIColor.darkGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    let stockTextField: UITextField = {
+        let textField = UITextField(frame: .zero)
+        textField.backgroundColor = UIColor.groupTableViewBackground
+        textField.layer.cornerRadius = 10
+        textField.clipsToBounds = true
+        textField.keyboardType = .default
+        textField.autocapitalizationType = .words
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    let quantityStackView: UIStackView = {
+        let stackView = UIStackView(frame: .zero)
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.spacing = 5
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    let quantityLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.text = "Quantidade"
+        label.numberOfLines = 1
+        label.textAlignment = .left
+        label.textColor = UIColor.darkGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    let quantityTextField: UITextField = {
+        let textField = UITextField(frame: .zero)
+        textField.backgroundColor = UIColor.groupTableViewBackground
+        textField.layer.cornerRadius = 10
+        textField.clipsToBounds = true
+        textField.keyboardType = .decimalPad
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    let priceStackView: UIStackView = {
+        let stackView = UIStackView(frame: .zero)
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.spacing = 5
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    let priceLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.text = "Preço de Compra"
+        label.numberOfLines = 1
+        label.textAlignment = .left
+        label.textColor = UIColor.darkGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    let priceTextField: UITextField = {
+        let textField = UITextField(frame: .zero)
+        textField.backgroundColor = UIColor.groupTableViewBackground
+        textField.layer.cornerRadius = 10
+        textField.clipsToBounds = true
+        textField.keyboardType = .decimalPad
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    let startDateStackView: UIStackView = {
+        let stackView = UIStackView(frame: .zero)
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.spacing = 5
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    let startDateLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.text = "Preço de Compra"
+        label.numberOfLines = 1
+        label.textAlignment = .left
+        label.textColor = UIColor.darkGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    let startDateTextField: UITextField = {
+        let textField = UITextField(frame: .zero)
+        textField.backgroundColor = UIColor.groupTableViewBackground
+        textField.layer.cornerRadius = 10
+        textField.clipsToBounds = true
+        textField.keyboardType = .decimalPad
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    let textsStackView: UIStackView = {
+        let stackView = UIStackView(frame: .zero)
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        stackView.spacing = 16
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    let investOrSaveButton: GradientButton = {
+        let button = GradientButton()
+        button.setTitle("Investir", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    // MARK: - Inits
+    convenience init(delegate: AddOrEditStockViewDelegate){
+        self.init()
+        self.delegate = delegate
+    }
+    
+    override func initialize() {
+        contentView.backgroundColor = UIColor.white
+        addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        stockStackView.addArrangedSubview(stockLabel)
+        stockStackView.addArrangedSubview(stockTextField)
+        quantityStackView.addArrangedSubview(quantityLabel)
+        quantityStackView.addArrangedSubview(quantityTextField)
+        priceStackView.addArrangedSubview(priceLabel)
+        priceStackView.addArrangedSubview(priceTextField)
+        startDateStackView.addArrangedSubview(startDateLabel)
+        startDateStackView.addArrangedSubview(startDateTextField)
+        textsStackView.addArrangedSubview(stockStackView)
+        textsStackView.addArrangedSubview(quantityStackView)
+        textsStackView.addArrangedSubview(priceStackView)
+        textsStackView.addArrangedSubview(startDateStackView)
+        contentView.addSubview(closeButton)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(textsStackView)
+        contentView.addSubview(investOrSaveButton)
+    }
+    
+    override func installConstraints() {
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            scrollView.widthAnchor.constraint(equalTo: widthAnchor),
+            scrollView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            //Content View
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            
+            titleLabel.topAnchor.constraint(equalTo: closeButton.bottomAnchor, constant: 16),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            
+            textsStackView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 24),
+            textsStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            textsStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            
+            investOrSaveButton.topAnchor.constraint(greaterThanOrEqualTo: textsStackView.bottomAnchor, constant: -24),
+            investOrSaveButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -24),
+            investOrSaveButton.heightAnchor.constraint(equalToConstant: 50),
+            investOrSaveButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            investOrSaveButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            
+            closeButton.heightAnchor.constraint(equalToConstant: 40),
+            closeButton.widthAnchor.constraint(equalToConstant: 40),
+            closeButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            closeButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            
+            stockTextField.heightAnchor.constraint(equalToConstant: 40),
+            quantityTextField.heightAnchor.constraint(equalToConstant: 40),
+            priceTextField.heightAnchor.constraint(equalToConstant: 40),
+            startDateTextField.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        let contentViewHeightContraint = contentView.heightAnchor.constraint(equalTo: scrollView.heightAnchor)
+        contentViewHeightContraint.priority = .defaultLow
+        contentViewHeightContraint.isActive = true
+    }
+    
+    override func setupExtraConfigurations() {
+        closeButton.addTarget(self, action: #selector(closeTapped), for: .touchUpInside)
+        investOrSaveButton.addTarget(self, action: #selector(verifyErrorTapped), for: .touchUpInside)
+    }
+    
+    @objc private func closeTapped() {
+        delegate?.didTapCloseButton()
+    }
+    @objc private func verifyErrorTapped() {
+        verifyError()
+    }
+    
+    func verifyError() {
+        stockLabel.textColor = stockTextField.text?.isEmpty ?? false ? UIColor.red : UIColor.gray
+        startDateLabel.textColor = startDateTextField.text?.isEmpty ?? false ? UIColor.red : UIColor.gray
+        priceLabel.textColor = priceTextField.text?.isEmpty ?? false ? UIColor.red : UIColor.gray
+        quantityLabel.textColor = quantityTextField.text?.isEmpty ?? false ? UIColor.red : UIColor.gray
+        if  !(stockTextField.text?.isEmpty ?? false) && !(quantityTextField.text?.isEmpty ?? false) && !(priceTextField.text?.isEmpty ?? false) && !(startDateTextField.text?.isEmpty ?? false) {
+            delegate?.didTapInvestOrSaveButton()
+        }
+    }
+    func setSaveButton() {
+        investOrSaveButton.setTitle("Save", for: .normal)
+    }
+}
--- DevBoostItau-Project1/Scenas/AddOrEditStock/AddOrEditStockViewController.swift
+++ DevBoostItau-Project1/Scenas/AddOrEditStock/AddOrEditStockViewController.swift
@@ -8,36 +8,33 @@
 
 import UIKit
 
-final class AddOrEditStockViewController: BaseViewController {
-    // MARK: - Outlets
-    @IBOutlet weak var startDateTextField: UITextField!
-    @IBOutlet weak var stockTextField: UITextField!
-    @IBOutlet weak var quantityTextField: UITextField!
-    @IBOutlet weak var priceTextField: UITextField!
-    @IBOutlet weak var investButton: UIButton!
-    @IBOutlet weak var stockLabel: UILabel!
-    @IBOutlet weak var quantityLabel: UILabel!
-    @IBOutlet weak var startDateLabel: UILabel!
-    @IBOutlet weak var priceLabel: UILabel!
-
-    
+final class AddOrEditStockViewController: UIViewController, HasCodeView {
     // MARK: - Properts
-    private var datePicker = UIDatePicker()
-    private var viewModel = AddOrEditStockViewModel()
-    // MARK: - Init
-    convenience init(investment: Investment) {
-        self.init()
-        viewModel.investment = investment
+    typealias CustomView = AddOrEditStockView
+    weak var coordinator: AddOrEditStockCoordinator?
+    
+    // MARK: - Lifecycle
+    override func loadView() {
+        let customView = CustomView(delegate: self)
+        view = customView
     }
-
+    // MARK: - Properts
+    private var datePicker: UIDatePicker?
+    private let viewModel = AddOrEditStockViewModel()
+    
+    // MARK: - Init
+    convenience init(stock: Stock) {
+        self.init()
+        self.viewModel.stock = stock
+    }
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewModel.context = context
         setupDatePicker()
-        setupDataInViews()
-        setupViews()
-        addKeyboarController(for: [startDateTextField, stockTextField, quantityTextField, priceTextField])
+        if viewModel.stock != nil {
+            setupDateAndViews()
+        }
+        addKeyboarController(for: [customView.startDateTextField, customView.stockTextField, customView.quantityTextField, customView.priceTextField])
     }
     // MARK: - Override
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -46,77 +43,37 @@ final class AddOrEditStockViewController: BaseViewController {
     }
     // MARK: - Setups
     private func setupDatePicker() {
-        datePicker.datePickerMode = .date
-        datePicker.locale = Locale.init(identifier: "pt-br")
-        datePicker.addTarget(self, action: #selector(dateChanged), for: .valueChanged)
-        startDateTextField.inputView = datePicker
+        datePicker = UIDatePicker()
+        datePicker?.datePickerMode = .date
+        datePicker?.locale = Locale.init(identifier: "pt-br")
+        datePicker?.addTarget(self, action: #selector(dateChanged(datePicker:)), for: .valueChanged)
+        customView.startDateTextField.inputView = datePicker
     }
-    private func setupDataInViews() {
-        if let investment = viewModel.investment {
-            startDateTextField.text = investment.purchaseDate?.description
-            stockTextField.text = investment.brokerName!.isEmpty ? investment.brokerCode : investment.brokerName
-            quantityTextField.text = "\(investment.quantityOfStocks)"
-            priceTextField.text = "\(investment.purchasePrice)"
-        }
-        setupButton()
+    private func setupDateAndViews() {
+        customView.startDateTextField.text = viewModel.stock?.startDate
+        customView.stockTextField.text = viewModel.stock?.name
+        customView.quantityTextField.text = viewModel.stock?.quantity
+        customView.priceTextField.text = viewModel.stock?.price
+        customView.setSaveButton()
     }
-    private func setupViews() {
-        priceTextField.keyboardType = .decimalPad
-        quantityTextField.keyboardType = .decimalPad
-        stockTextField.keyboardType = .default
-        stockTextField.autocapitalizationType = .words
-        investButton.applyGradient(style: .horizontal, colors: [UIColor.itiOrange, UIColor.itiPink])
-        investButton.clipsToBounds = true
-        investButton.layer.cornerRadius = 25
-    }
-    private func setupButton() {
-        if viewModel.investment != nil {
-            investButton.setTitle("Salvar", for: .normal)
-        } else {
-            investButton.setTitle("Investir", for: .normal)
-        }
-    }
-    
     // MARK: - Taps
     @objc private func dateChanged(datePicker: UIDatePicker) {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd/MM/yyyy"
-        startDateTextField.text = dateFormatter.string(from: datePicker.date)
-    }
-    @IBAction func didTapInvestButton(_ sender: Any) {
-        if (validateFields()) {
-            callSaveInfo()
-        }
-    }
-    @IBAction func didTapCloseButton(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+        // startDateTextField.text = dateFormatter.string(from: datePicker.date)
     }
     
     // MARK: - Functions
-    private func validateFields() -> Bool {
-        stockLabel.textColor = stockTextField.text?.isEmpty ?? false ? UIColor.red : UIColor.gray
-        startDateLabel.textColor = startDateTextField.text?.isEmpty ?? false ? UIColor.red : UIColor.gray
-        priceLabel.textColor = priceTextField.text?.isEmpty ?? false ? UIColor.red : UIColor.gray
-        quantityLabel.textColor = quantityTextField.text?.isEmpty ?? false ? UIColor.red : UIColor.gray
-        if  !(stockTextField.text?.isEmpty ?? true) && !(quantityTextField.text?.isEmpty ?? true) && !(priceTextField.text?.isEmpty ?? true) && !(startDateTextField.text?.isEmpty ?? true) {
-            return true
-        }
-        return false
-    }
-    
     private func callSaveInfo() {
-        let brokerName = ""
-        let brokerCode = stockTextField.text ?? ""
-        let purchaseDate = startDateTextField.text ?? ""
-        let purchasePrice = priceTextField.text ?? "0"
-        let purchaseQuantity = quantityTextField.text ?? "0"
-        let saveSuccess = viewModel.saveInfo(brokerName: brokerCode, //TODO: não tem field para brokerName então estou setando brokerCode no brokerName mesmo
-                                             brokerCode: brokerCode,
-                                             qtyOfStocks: purchaseQuantity,
-                                             purchasePrice: purchasePrice,
-                                             purchaseDate: purchaseDate)
-        if (saveSuccess) {
-            dismiss(animated: true, completion: nil)
-        }
+        viewModel.saveInfo()
+    }
+}
+// MARK: - AddOrEditStockViewDelegate
+extension AddOrEditStockViewController: AddOrEditStockViewDelegate {
+    func didTapCloseButton() {
+        self.coordinator?.dismissDisplay()
+    }
+    func didTapInvestOrSaveButton() {
+        callSaveInfo()
     }
 }
