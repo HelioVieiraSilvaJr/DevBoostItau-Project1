7c86a3eef8df994c3bcabb104abbfeb303477b9d
Helio Junior <helio.engenharia@gmail.com>
2020-09-12 10:12:57 -0300

    added base cordinator

--- DevBoostItau-Project1/Sources/AppDelegate.swift
+++ DevBoostItau-Project1/Sources/AppDelegate.swift
@@ -15,26 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let viewController = UIStoryboard(name: "HomeViewController", bundle: nil)
-            .instantiateViewController(withIdentifier: "NavHomeViewController")
-        window?.rootViewController = viewController
-        window?.makeKeyAndVisible()
+        setupRootViewController()
         
         return true
     }
 
     // MARK: - Core Data stack
-
     lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-        */
+
         let container = NSPersistentContainer(name: "DevBoostItau_Project1")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
@@ -43,4 +32,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         })
         return container
     }()
+}
+
+extension AppDelegate {
+    func setupRootViewController() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+                
+                //DEPRECATED
+                let viewController = UIStoryboard(name: "HomeViewController", bundle: nil)
+                    .instantiateViewController(withIdentifier: "NavHomeViewController")
+                window?.rootViewController = viewController
+                
+                //SUBSTITUIR POR CORDINATOR
+        //        appCoordinator = AppCoordinator()
+        //        window?.rootViewController = appCoordinator?.navigationController
+        //        appCoordinator?.start()
+                
+                window?.makeKeyAndVisible()
+    }
 }
---
+++ DevBoostItau-Project1/Sources/Cordinator/Cordinator.swift
@@ -0,0 +1,29 @@
+//
+//  Cordinator.swift
+//  DevBoostItau-Project1
+//
+//  Created by Helio Junior on 12/09/20.
+//  Copyright © 2020 DevBoost-Itau. All rights reserved.
+//
+
+import UIKit
+
+protocol Coordinator: AnyObject {
+    
+    var navigationController: UINavigationController {get set}
+    var childCoordinators: [Coordinator] {get set}
+    var parentCoordinator: Coordinator? {get set}
+    
+    func start()
+    func add(childCoordinator coordinator: Coordinator)
+    func remove(childCoordinator coordinator: Coordinator)
+}
+
+extension Coordinator {
+    func add(childCoordinator coordinator: Coordinator) {
+        childCoordinators.append(coordinator)
+    }
+    func remove(childCoordinator coordinator: Coordinator) {
+        childCoordinators = childCoordinators.filter{$0 !== coordinator}
+    }
+}
