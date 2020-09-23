59a2430c052e61c3ff746b0cbd8937c8ad703a5f
Gabriel den Hartog <gabriel1786@gmail.com>
2020-09-23 19:46:18 -0300

    FirebaseAuth initialization, added GoogleService-Info.plist

---
+++ DevBoostItau-Project1/Shared/Managers/AuthManager.swift
@@ -0,0 +1,21 @@
+//
+//  AuthManager.swift
+//  DevBoostItau-Project1
+//
+//  Created by Gabriel den Hartog on 23/09/20.
+//  Copyright © 2020 DevBoost-Itau. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthManager {
+    
+    func isLoggedIn() -> Bool {
+        return true
+    }
+    
+    func performLoginWith(username: String, password: String) -> Bool {
+        return true
+    }
+}
