a938e9fb8bccc7f18bbbe05e1b1e9845067588f0
Gabriel den Hartog <gabriel1786@gmail.com>
2020-09-23 20:03:44 -0300

    Validating user login status on AppCoordinator

--- DevBoostItau-Project1/Shared/Managers/AuthManager.swift
+++ DevBoostItau-Project1/Shared/Managers/AuthManager.swift
@@ -11,8 +11,29 @@ import Firebase
 
 class AuthManager {
     
+    // MARK: - Properties
+    static let shared = AuthManager()
+    
+    private init() {
+    }
+    
+    // MARK: - Methods
     func isLoggedIn() -> Bool {
-        return true
+        if let _ = Auth.auth().currentUser {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func performLogout() -> Bool {
+        do {
+            try Auth.auth().signOut()
+            return true
+        } catch {
+            print("Could not sign out user")
+            return false
+        }
     }
     
     func performLoginWith(username: String, password: String) -> Bool {
